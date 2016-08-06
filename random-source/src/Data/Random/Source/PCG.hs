6148e2eaae7dd2226da49b57c3679bc9b693807d
Dominic Steinitz <dominic@steinitz.org>
2016-08-06 07:13:02 +0100

    A possible use of PCG.

---
+++ random-source/src/Data/Random/Source/PCG.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE GADTs                 #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+-- |This module defines the following instances:
+--
+-- > instance RandomSource (ST s) (Gen s)
+-- > instance RandomSource IO (Gen RealWorld)
+module Data.Random.Source.PCG
+    ( Gen, RealWorld
+    , create, initialize
+    , save, restore
+    ) where
+
+import Data.Random.Internal.Words
+import Data.Random.Source
+import System.Random.PCG
+import Control.Monad.ST
+import Control.Monad.Reader
+import Control.Monad.Primitive
+
+$(randomSource
+    [d| instance RandomSource (ST s) (Gen s) where
+            getRandomWord8From  = uniform
+            getRandomWord16From = uniform
+            getRandomWord32From = uniform
+            getRandomWord64From = uniform
+            getRandomDoubleFrom = fmap wordToDouble . uniform
+     |])
+
+$(randomSource
+    [d| instance RandomSource IO (Gen RealWorld) where
+            getRandomWord8From  = uniform
+            getRandomWord16From = uniform
+            getRandomWord32From = uniform
+            getRandomWord64From = uniform
+            getRandomDoubleFrom = fmap wordToDouble . uniform
+     |])
+
+$(monadRandom [d|
+  instance (PrimMonad m, s ~ PrimState m) => MonadRandom (ReaderT (Gen s) m) where
+    getRandomWord16 = ask >>= lift . uniform
+    getRandomWord32 = ask >>= lift . uniform
+    getRandomWord64 = ask >>= lift . uniform
+  |])
